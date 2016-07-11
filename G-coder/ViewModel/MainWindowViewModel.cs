119bce1960a6e063206abf5d9a4a094507443572
Michał <gorczix@gmail.com>
2016-07-11 15:37:44 +0200

    added MainWindowViewModel and RelayCommand classes

---
+++ G-coder/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,53 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+using G_coder.Annotations;
+using Microsoft.Win32;
+
+namespace G_coder.ViewModel
+{
+    internal class MainWindowViewModel : INotifyPropertyChanged
+    {
+        private string _path;
+        public event PropertyChangedEventHandler PropertyChanged;
+        
+        public string Path
+        {
+            get { return _path; }
+            set
+            {
+                _path = value;
+                OnPropertyChanged(nameof(Path));
+            }
+        }
+
+        public ICommand OpenFile
+        {
+            get { return new RelayCommand(OpenFileExecute, CanOpenFileExecute); }
+        }
+        
+        public MainWindowViewModel()
+        {
+
+        }
+
+        public void OpenFileExecute()
+        {
+            var ofd = new OpenFileDialog();
+            var result = ofd.ShowDialog();
+            if (result == true)
+                Path = ofd.SafeFileName;
+        }
+
+        private bool CanOpenFileExecute()
+        {
+            return true;
+        }
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ G-coder/ViewModel/RelayCommand.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace G_coder.ViewModel
+{
+    internal class RelayCommand : ICommand
+    {
+        private Func<bool> _canExecute;
+        private Action _execute;
+
+        public RelayCommand(Action execute)
+            : this(execute, null)
+        {
+            
+        }
+
+        public RelayCommand(Action execute, Func<bool> canExecute)
+        {
+            if(execute == null)
+                throw new ArgumentNullException(nameof(execute));
+            this._execute = execute;
+            this._canExecute = canExecute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute == null ? true : _canExecute();
+        }
+
+        public void Execute(object parameter)
+        {
+            _execute();
+        }
+
+        public event EventHandler CanExecuteChanged;
+    }
+}
